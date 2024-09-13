a9edea7d140e3018f988368f8e1f38d7bc82bbaa
Ruben <66105177+rubengm13@users.noreply.github.com>
2024-09-13 13:32:47 -0700

    Update main.tf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -41,3 +41,26 @@ resource "kubernetes_pod" "test-2" {
     }
   }
 }
+
+
+resource "kubernetes_pod" "test-3" {
+  metadata {
+    name = "terraform-example"
+  }
+
+  spec {
+    container {
+      image = "nginx:1.21.6"
+      name  = "example"
+
+      env {
+        name  = "environment"
+        value = "test"
+      }
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
